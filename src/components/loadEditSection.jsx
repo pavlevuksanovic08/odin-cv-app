c95bc883f8df348340488feca51ef2fd84e11d64
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-01-04 14:52:07 +0100

    load more fieldsets

--- src/components/loadEditSection.jsx
+++ src/components/loadEditSection.jsx
@@ -4,11 +4,13 @@ import EditBox from "./editBox";
 export default function LoadEditSection({ section }) {
     return (
         <div>
-            {section.fields.map((field, index) => (
-                <div key={index}>
-                    <EditBox data={field} />
-                </div>
-            ))}
+            {section.fields.map((field) => (
+                field.map((input, index) => (
+                    <div key={index}>
+                        <EditBox data={input} />
+                    </div>
+                )              
+            )))}
         </div>
     )
 }
